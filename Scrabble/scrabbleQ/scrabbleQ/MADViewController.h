263f04055fb203c41962b8f2774354e2078f49f3
Guest User <Guest@Marios-MacBook-Pro.local>
2013-12-11 17:48:22 -0700

    Correction

---
+++ Scrabble/scrabbleQ/scrabbleQ/MADViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADViewController.h
+//  scrabbleQ
+//
+//  Created by Luis Alfredo Hierro on 10/22/13.
+//  Copyright (c) 2013 Luis Alfredo Hierro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
